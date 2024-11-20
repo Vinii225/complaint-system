508b86a89d8a757e922cc8fa1eb27590c8a58107
woragis <masteringthecode.woragis+github@gmail.com>
2024-11-20 15:23:23 -0300

    Redesing home page

--- index.php
+++ index.php
@@ -5,23 +5,17 @@ include $_SERVER['DOCUMENT_ROOT'] . '/includes/header.php';
 ?>
 
 <section class="home">
-  <div class="container">
-    <h1>Ouvidoria Online</h1>
-    <?php
+  <h1>Ouvidoria Online</h1>
+  <?php
+  if (isset($_GET['id'])) {
+    $post_id = (int)$_GET['id'];
+    include $_SERVER['DOCUMENT_ROOT'] . '/includes/post_read.php';
+  } else {
     include $_SERVER['DOCUMENT_ROOT'] . '/includes/post_create.php';
-    ?>
-    <div>
+    include $_SERVER['DOCUMENT_ROOT'] . '/includes/posts_read.php';
+  }
+  ?>
 </section>
-
-<?php
-if (isset($_GET['id'])) {
-  $post_id = (int)$_GET['id']; // Sanitizar o id da URL
-  include $_SERVER['DOCUMENT_ROOT'] . '/includes/post_read.php';
-} else {
-  include $_SERVER['DOCUMENT_ROOT'] . '/includes/posts_read.php';
-}
-?>
-
 
 <?php
 include $_SERVER['DOCUMENT_ROOT'] . '/includes/footer.php';
